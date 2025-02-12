849501412ac9cf81893e4664b9a55a0de4782a02
RaceProUK <raceprouk@hotmail.co.uk>
2025-02-12 09:22:21 +0000

    Started work on Tails Adventure (BizHawk) Client

---
+++ worlds/tailsadv/Client.py
@@ -0,0 +1,41 @@
+import base64
+import logging
+import time
+
+from NetUtils import ClientStatus
+from worlds._bizhawk.client import BizHawkClient
+from worlds._bizhawk import read, write, guarded_write
+
+from .Locations import location_data
+from .ROM import ROMType
+
+logger = logging.getLogger("Client")
+
+class TailsAdvClient(BizHawkClient):
+    system = "GG"
+    patch_suffix = ".aptailsadv"
+    game = "Tails Adventure"
+
+    async def validate_rom(self, ctx) -> bool:
+        print(ctx.rom_hash)
+        if ctx.rom_hash in [
+            ROMType.Original.value,
+            ROMType.VC3DS.value,
+            ROMType.Origins.value
+        ]:
+            ctx.game = self.game
+            ctx.items_handling = 0b111
+            ctx.finished_game = False
+            return True
+        return False
+    
+    def on_package(self, ctx, cmd, args) -> None:
+        if cmd == "RoomInfo":
+            ctx.seed_name = args["seed_name"]
+
+    async def set_auth(self, ctx) -> None:
+        ctx.auth = self.player_name
+
+    async def game_watcher(self, ctx) -> None:
+        if not ctx.server or not ctx.server.socket.open or ctx.server.socket.closed:
+            return
