bdbce055a1dfdbc6039ce3ff94c8fe24984c0c5f
RaceProUK <raceprouk@hotmail.co.uk>
2025-02-13 20:24:43 +0000

    Location checks and received items are now processed (also some refactoring)

--- worlds/tailsadv/Client.py
+++ worlds/tailsadv/Client.py
@@ -4,9 +4,13 @@ from enum import Enum
 
 from NetUtils import ClientStatus
 from worlds._bizhawk.client import BizHawkClient
+from .Items import item_data_table
 from .ROM import ROMType
 
 import worlds._bizhawk as bizhawk
+
+def stored_data_key(team: int, slot: int, key: str) -> str:
+    return f"tailsadv_{team}_{slot}_{key}"
 
 logger = logging.getLogger("Client")
 
@@ -40,6 +44,10 @@ session_state_data_locations = {
     DataKeys.ItemPickup: (0x12ab, 0x01)
 }
 
+item_page_bit_map = {
+    data.id: (data.page, data.bit) for data in item_data_table.values()
+}
+
 class TailsAdvClient(BizHawkClient):
     system = "GG"
     patch_suffix = ".aptailsadv"
@@ -66,10 +74,10 @@ class TailsAdvClient(BizHawkClient):
                 ctx.seed_name = args["seed_name"]
             case "Connected":
                 ctx.set_notify([
-                    f"tailsadv_{ctx.slot}_{ctx.team}_{DataKeys.ItemsPage1.value()}",
-                    f"tailsadv_{ctx.slot}_{ctx.team}_{DataKeys.ItemsPage2.value()}",
-                    f"tailsadv_{ctx.slot}_{ctx.team}_{DataKeys.ItemsPage3.value()}",
-                    f"tailsadv_{ctx.slot}_{ctx.team}_{DataKeys.ItemsPageSub.value()}"
+                    stored_data_key(ctx.team, ctx.slot, DataKeys.ItemsPage1.value()),
+                    stored_data_key(ctx.team, ctx.slot, DataKeys.ItemsPage2.value()),
+                    stored_data_key(ctx.team, ctx.slot, DataKeys.ItemsPage3.value()),
+                    stored_data_key(ctx.team, ctx.slot, DataKeys.ItemsPageSub.value())
                 ])
 
     async def game_watcher(self, ctx) -> None:
@@ -99,10 +107,10 @@ class TailsAdvClient(BizHawkClient):
 
         # Ensure game inventory is correct when on map screen        
         if ctx.room_id != room_id and room_id == WORLD_MAP_ID:
-            items_page_1 = int(ctx.stored_data.get(f"tailsadv_{ctx.slot}_{ctx.team}_{DataKeys.ItemsPage1.value()}", 0))
-            items_page_2 = int(ctx.stored_data.get(f"tailsadv_{ctx.slot}_{ctx.team}_{DataKeys.ItemsPage2.value()}", 0))
-            items_page_3 = int(ctx.stored_data.get(f"tailsadv_{ctx.slot}_{ctx.team}_{DataKeys.ItemsPage3.value()}", 0))
-            items_page_sub = int(ctx.stored_data.get(f"tailsadv_{ctx.slot}_{ctx.team}_{DataKeys.ItemsPageSub.value()}", 0))
+            items_page_1 = int(ctx.stored_data.get(stored_data_key(ctx.team, ctx.slot, DataKeys.ItemsPage1.value()), 0))
+            items_page_2 = int(ctx.stored_data.get(stored_data_key(ctx.team, ctx.slot, DataKeys.ItemsPage2.value()), 0))
+            items_page_3 = int(ctx.stored_data.get(stored_data_key(ctx.team, ctx.slot, DataKeys.ItemsPage3.value()), 0))
+            items_page_sub = int(ctx.stored_data.get(stored_data_key(ctx.team, ctx.slot, DataKeys.ItemsPageSub.value()), 0))
             keys = [(loc_data[0], loc_data[1], "RAM")
                     for loc_data in persisted_state_data_locations.values()]
             data = [items_page_1, items_page_2, items_page_3, items_page_sub]
@@ -110,19 +118,42 @@ class TailsAdvClient(BizHawkClient):
 
         # Process location check
         if item_pickup == SENTINEL_VALUE and item_obtained:
-            # TODO: Implement location check
-            pass
+            # The Chaos Emeralds have IDs from 27 to 32 in AP and 32 to 37 in the game, hence the conversion
+            await ctx.send_msgs([{
+                "cmd": "LocationChecks",
+                "locations": [item_obtained - 5 if 32 <= item_obtained <= 37 else item_obtained]
+            }])
 
         # Process received items
         while ctx.current_index < len(ctx.items_received):
-            # TODO: Implement receiving item
+            id = ctx.items_received[ctx.current_index].item
             ctx.current_index += 1
 
-        # Save live state data on the server        
-        def set_data_message(key: str, value: int):
+            page, bit = item_page_bit_map[id]
+            match page:
+                case 1:
+                    page = DataKeys.ItemsPage1.value()
+                case 2:
+                    page = DataKeys.ItemsPage2.value()
+                case 3:
+                    page = DataKeys.ItemsPage3.value()
+                case 4:                    
+                    page = DataKeys.ItemsPageSub.value()
+                case _:
+                    return
+            await ctx.send_msgs([{
+                "cmd": "Set",
+                "key": stored_data_key(ctx.team, ctx.slot, page),
+                "default": 0,
+                "want_reply": True,
+                "operations": [{ "operation": "or", "value": (1 << bit) }]
+            }])
+
+        # Save session state data on the server        
+        def set_data_message(key: str, value: int) -> None:
             return {
                 "cmd": "Set",
-                "key": f"tailsadv_{ctx.slot}_{ctx.team}_{key}",
+                "key": stored_data_key(ctx.team, ctx.slot, key),
                 "default": 0,
                 "want_reply": False,
                 "operations": [{ "operation": "replace", "value": value }]
--- worlds/tailsadv/Items.py
+++ worlds/tailsadv/Items.py
@@ -7,44 +7,46 @@ class TailsAdvItem(Item):
 
 class TailsAdvItemData(NamedTuple):
     code: int
+    page: int
+    bit: int
     type: ItemClassification
     can_create: bool = True
     start_with: bool = False
 
 item_data_table: Dict[str, TailsAdvItemData] = {
-    "Regular Bomb": TailsAdvItemData(1, ItemClassification.progression, can_create=False, start_with=True),
-    "Large Bomb": TailsAdvItemData(2, ItemClassification.progression),
-    "Remote Bomb": TailsAdvItemData(3, ItemClassification.progression),
-    "Napalm Bomb": TailsAdvItemData(4, ItemClassification.progression),
-    "Triple Bomb": TailsAdvItemData(5, ItemClassification.useful),
-    "Wrench": TailsAdvItemData(6, ItemClassification.useful),
-    "Helmet": TailsAdvItemData(7, ItemClassification.useful),
-    "Item Radar": TailsAdvItemData(8, ItemClassification.useful),
-    "Radio": TailsAdvItemData(9, ItemClassification.useful),
-    "Hammer": TailsAdvItemData(10, ItemClassification.useful),
-    "Teleport Device": TailsAdvItemData(11, ItemClassification.useful),
-    "Night Vision": TailsAdvItemData(12, ItemClassification.progression),
-    "Speed Boots": TailsAdvItemData(13, ItemClassification.useful),
-    "Super Glove": TailsAdvItemData(14, ItemClassification.useful),
-    "Fang": TailsAdvItemData(15, ItemClassification.useful),
-    "Knuckles": TailsAdvItemData(16, ItemClassification.useful),
-    "Sonic": TailsAdvItemData(17, ItemClassification.useful),
-    "Proton Torpedo": TailsAdvItemData(18, ItemClassification.progression),
-    "Vulcan Gun": TailsAdvItemData(19, ItemClassification.progression, can_create=False, start_with=True),
-    "Extra Speed": TailsAdvItemData(20, ItemClassification.progression),
-    "Extra Armor": TailsAdvItemData(21, ItemClassification.progression),
-    "Anti Air Missile": TailsAdvItemData(22, ItemClassification.progression),
-    "Spark": TailsAdvItemData(23, ItemClassification.useful),
-    "Mine": TailsAdvItemData(24, ItemClassification.progression),
-    "Rocket Booster": TailsAdvItemData(25, ItemClassification.progression),
-    "Remote Robot": TailsAdvItemData(26, ItemClassification.progression),
-    "Blue Chaos Emerald": TailsAdvItemData(27, ItemClassification.useful),
-    "Green Chaos Emerald": TailsAdvItemData(28, ItemClassification.useful),
-    "Purple Chaos Emerald": TailsAdvItemData(29, ItemClassification.useful),
-    "Red Chaos Emerald": TailsAdvItemData(30, ItemClassification.useful),
-    "White Chaos Emerald": TailsAdvItemData(31, ItemClassification.useful),
-    "Yellow Chaos Emerald": TailsAdvItemData(32, ItemClassification.useful),
-    "Ring": TailsAdvItemData(33, ItemClassification.filler, can_create=False)
+    "Regular Bomb": TailsAdvItemData(1, 1, 7, ItemClassification.progression, can_create=False, start_with=True),
+    "Large Bomb": TailsAdvItemData(2, 1, 6, ItemClassification.progression),
+    "Remote Bomb": TailsAdvItemData(3, 1, 5, ItemClassification.progression),
+    "Napalm Bomb": TailsAdvItemData(4, 1, 4, ItemClassification.progression),
+    "Triple Bomb": TailsAdvItemData(5, 1 ,3, ItemClassification.useful),
+    "Wrench": TailsAdvItemData(6, 1, 2, ItemClassification.useful),
+    "Helmet": TailsAdvItemData(7, 1, 1, ItemClassification.useful),
+    "Item Radar": TailsAdvItemData(8, 3, 7, ItemClassification.useful),
+    "Radio": TailsAdvItemData(9, 3, 6, ItemClassification.useful),
+    "Hammer": TailsAdvItemData(10, 2, 7, ItemClassification.useful),
+    "Teleport Device": TailsAdvItemData(11, 2, 6, ItemClassification.useful),
+    "Night Vision": TailsAdvItemData(12, 2, 5, ItemClassification.progression),
+    "Speed Boots": TailsAdvItemData(13, 2, 4, ItemClassification.useful),
+    "Super Glove": TailsAdvItemData(14, 2, 3, ItemClassification.useful),
+    "Fang": TailsAdvItemData(15, 2, 2, ItemClassification.useful),
+    "Knuckles": TailsAdvItemData(16, 2, 1, ItemClassification.useful),
+    "Sonic": TailsAdvItemData(17, 2, 0, ItemClassification.useful),
+    "Proton Torpedo": TailsAdvItemData(18, 4, 7, ItemClassification.progression),
+    "Vulcan Gun": TailsAdvItemData(19, 4, 6, ItemClassification.progression, can_create=False, start_with=True),
+    "Extra Speed": TailsAdvItemData(20, 4, 5, ItemClassification.progression),
+    "Extra Armor": TailsAdvItemData(21, 4, 4, ItemClassification.progression),
+    "Anti Air Missile": TailsAdvItemData(22, 4, 3, ItemClassification.progression),
+    "Spark": TailsAdvItemData(23, 4, 2, ItemClassification.useful),
+    "Mine": TailsAdvItemData(24, 4, 1, ItemClassification.progression),
+    "Rocket Booster": TailsAdvItemData(25, 4, 0, ItemClassification.progression),
+    "Remote Robot": TailsAdvItemData(26, 1, 0, ItemClassification.progression),
+    "Blue Chaos Emerald": TailsAdvItemData(27, 3, 5, ItemClassification.useful),
+    "Green Chaos Emerald": TailsAdvItemData(28, 3, 4, ItemClassification.useful),
+    "Purple Chaos Emerald": TailsAdvItemData(29, 3, 3, ItemClassification.useful),
+    "Red Chaos Emerald": TailsAdvItemData(30, 3, 2, ItemClassification.useful),
+    "White Chaos Emerald": TailsAdvItemData(31, 3, 1, ItemClassification.useful),
+    "Yellow Chaos Emerald": TailsAdvItemData(32, 3, 0, ItemClassification.useful),
+    "Ring": TailsAdvItemData(33, 0, 0, ItemClassification.filler, can_create=False)
 }
 
 item_table = {
